8d847eef161b72b000a993e2a82ba3d4034aaafc
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-02-20 23:47:37 -0800

    Generate readable names

--- scripts/dummy_data.py
+++ scripts/dummy_data.py
@@ -1,26 +1,34 @@
+from itertools import product
 import json
 import random
 from string import ascii_lowercase
 from uuid import uuid4
 
+COLUMN_NAMES = ["Backlog", "Todo", "Doing", "Stalled", "Done"]
+ADJECTIVES = ["Big", "Important", "Tricky", "UI", "Tiny"]
+NOUNS = ["bug fix", "feature", "task", "spike", "decision"]
+CARD_NAMES = [" ".join(words) for words in product(ADJECTIVES, NOUNS)]
+
+random.shuffle(CARD_NAMES)
+
 data = {"columns": {}, "cards": {}}
 
-for _ in range(5):
+for i in range(len(COLUMN_NAMES)):
     columnUuid = str(uuid4())
 
     data["columns"][columnUuid] = {
         "uuid": columnUuid,
-        "title": "".join(random.sample(ascii_lowercase, k=random.randint(5, 10))),
+        "title": COLUMN_NAMES[i],
         "cardUuids": [],
     }
 
-    for _ in range(5):
+    for j in range(len(ADJECTIVES)):
         cardUuid = str(uuid4())
 
         data["columns"][columnUuid]["cardUuids"].append(cardUuid)
         data["cards"][cardUuid] = {
             "uuid": cardUuid,
-            "title": "".join(random.sample(ascii_lowercase, k=random.randint(10, 20))),
+            "title": CARD_NAMES[i * 5 + j],
             "columnUuid": columnUuid,
         }
 
